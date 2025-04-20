19a299aca31e54d57acd0e9ec0e96f24944377c0
toyboot4e <toyboot4e@gmail.com>
2025-04-20 11:29:36 +0900

    wip

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,26 +1,26 @@
-import Versions from './components/Versions'
+export const Editor = (): React.JSX.Element => {
+  return (
+    <div className="ved-editor vert-mode multi-col-mode">
+      <div
+        id="editor-content"
+        className="ved-editor-content vert-mode multi-col-mode"
+        contentEditable="true"
+      ></div>
+    </div>
+  )
+}
 
-function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+export const App = (): React.JSX.Element => {
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
     <div className="ved">
       {/* Make space for trafic lights(macOS only) */}
       <div className="ved-header"></div>
-
-      <div className="ved-editor vert-mode multi-col-mode">
-        <div
-          id="editor-content"
-          className="ved-editor-content vert-mode multi-col-mode"
-          contentEditable="true"
-        ></div>
-      </div>
-
+      {Editor()}
       <div className="ved-footer">
         <p id="counter" className="ved-footer-counter"></p>
       </div>
     </div>
   )
 }
-
-export default App
