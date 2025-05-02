24ec5615d8d9eafc3ab7489d2e894a0a10a78eec
toyboot4e <toyboot4e@gmail.com>
2025-05-03 02:48:56 +0900

    wip Rich text

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,13 +4,13 @@ import { useState } from 'react'
 export const App = (): React.JSX.Element => {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
   const [dir, _setDir] = useState(WritingDirection.Vertical)
-  const [appearPolicy, _setAppearPolicy] = useState(AppearPolicy.ByCharacter)
+  const [appearPolicy, setAppearPolicy] = useState(AppearPolicy.Rich)
 
   return (
     <div className="ved">
       {/* Make space for trafic lights(macOS only) */}
       <div className="ved-header"></div>
-      {VedEditor({ dir, appearPolicy })}
+      {VedEditor({ dir, appearPolicy, setAppearPolicy })}
       <div className="ved-footer">
         <p id="counter" className="ved-footer-counter"></p>
       </div>
--- src/renderer/src/components/Editor.tsx
+++ src/renderer/src/components/Editor.tsx
@@ -1,7 +1,19 @@
 import { clsx } from 'clsx'
-import { createEditor, Editor, Path, Range, NodeEntry, Text, Transforms } from 'slate'
+import {
+  createEditor,
+  Descendant,
+  Editor,
+  Path,
+  Range,
+  Element,
+  BaseElement,
+  Node,
+  NodeEntry,
+  Text,
+  Transforms
+} from 'slate'
 import { withHistory } from 'slate-history'
-import { Slate, withReact, Editable, RenderLeafProps } from 'slate-react'
+import { Slate, withReact, Editable, RenderLeafProps, RenderElementProps } from 'slate-react'
 import { useState, useCallback } from 'react'
 
 // TODO: how to handle intersecting decorations
@@ -21,9 +33,21 @@ interface VedRange extends Range {
   format: Format
 }
 
-/** Parsed leaf `Text` with `Format`. */
-interface VedLeaf extends Text {
-  format?: Format
+type VedElement = BaseElement & { type?: VedElementType }
+
+type VedElementType = 'Ruby' | 'TODO'
+
+type RubyElement = {
+  type: 'Ruby'
+  rubyText: string
+  children: Descendant[]
+}
+
+type VedLeafType = 'RubyBody' | 'Rt'
+
+type VedLeaf = {
+  type: VedLeafType
+  text: string
 }
 
 const EditorUtil = {
@@ -56,13 +80,35 @@ const EditorUtil = {
   }
 }
 
-const decorateRubies = (editor: Editor, ranges: VedRange[], path: Path, text: string) => {
-  {
-    // debug
-    const { selection } = editor
-    console.log(selection, text)
+const parseFormats = (text: string): Format[] => {
+  const formats = []
+
+  let offset = 0
+  while (true) {
+    offset = text.indexOf('|', offset)
+    if (offset === -1) break
+
+    const l = text.indexOf('(', offset)
+    if (l === -1) break
+
+    const r = text.indexOf(')', l)
+    if (r === -1) break
+
+    formats.push({
+      delimFront: [offset, offset + 1],
+      text: [offset + 1, l],
+      sepMid: [l, l + 1],
+      rubyText: [l + 1, r],
+      delimEnd: [r, r + 1]
+    })
+
+    offset = r
   }
 
+  return formats
+}
+
+const decorateRubies = (editor: Editor, ranges: VedRange[], path: Path, text: string) => {
   let offset = 0
   while (true) {
     // TODO: portable, configurable format
@@ -103,12 +149,14 @@ const decorateImpl = (
   appearPolicy: AppearPolicy,
   [node, path]: NodeEntry
 ): VedRange[] => {
+  return []
+
   if (!Text.isText(node)) {
     return []
   }
 
   // by-paragraph appear
-  if (appearPolicy == AppearPolicy.ByParagraph && EditorUtil.intersects(editor, path)) {
+  if (appearPolicy === AppearPolicy.ByParagraph && EditorUtil.intersects(editor, path)) {
     return []
   }
 
@@ -127,42 +175,81 @@ const useDecorate = (editor: Editor, appearPolicy: AppearPolicy) => {
   )
 }
 
-const Leaf = ({ attributes, children, leaf: rawLeaf }: RenderLeafProps) => {
+/** Ved leaf component */
+const VedLeaf = ({ attributes, children, leaf: rawLeaf }: RenderLeafProps) => {
   const leaf = rawLeaf as VedLeaf
-  const returnDefault = () => <span {...attributes}>{children}</span>
 
-  if (leaf.format === undefined) {
-    return returnDefault()
+  console.log('leaf', leaf.type, leaf.text)
+  if (leaf.type === undefined) {
+    // FIXME: create a three nesting span
+    return <span {...attributes}>{children}</span>
   }
 
-  const offset = leaf.format.delimFront[0]
-  const leafText = leaf.text.substring(leaf.format.text[0] - offset, leaf.format.text[1] - offset)
-  const leafRuby = leaf.text.substring(
-    leaf.format.rubyText[0] - offset,
-    leaf.format.rubyText[1] - offset
-  )
+  switch (leaf.type) {
+    case 'RubyBody':
+      // TODO: <span> is added by Slate?
+      return leaf.text
+    case 'Rt':
+      return (
+        <>
+          <rp>(</rp>
+          <rt>{leaf.text}</rt>
+          <rp>)</rp>
+        </>
+      )
+  }
 
-  return (
-    <span {...attributes}>
-      <ruby>
-        {leafText}
-        <rp>(</rp>
-        <rt>{leafRuby}</rt>
-        <rp>)</rp>
-      </ruby>
-    </span>
-  )
+  // return (
+  //   <ruby {...attributes}>
+  //     {leafText}
+  //     <rp>(</rp>
+  //     <rt>{leafRuby}</rt>
+  //     <rp>)</rp>
+  //   </ruby>
+  // )
+}
+
+const InlineChromiumBugfix = () => (
+  <span contentEditable={false} style={{ fontSize: 0 }}>
+    {String.fromCodePoint(160) /* Non-breaking space */}
+  </span>
+)
+/** Ved element component. Note that `withInline` lets us insert `Ruby` as inline element. */
+const VedElement = ({ attributes, children, element: rawElement }: RenderElementProps) => {
+  const vedElement = rawElement as VedElement
+
+  // TODO: we could still use decorate??
+  console.log('elem', vedElement.type, vedElement)
+  switch (vedElement.type) {
+    case 'Ruby':
+      return (
+        <ruby {...attributes}>
+          {/*<InlineChromiumBugfix />*/}
+          {children}
+          <rp>(</rp>
+          <rt contentEditable={false}>振り仮名！</rt>
+          {/*<rt data-slate-node="text">振り仮名！</rt>*/}
+          <rp>)</rp>
+          {/*<InlineChromiumBugfix />*/}
+        </ruby>
+      )
+
+    default:
+      return <p {...attributes}>{children}</p>
+  }
 }
 
 const useOnKeyDown = (
   editor: Editor,
-  vert: boolean
+  vert: boolean,
+  toggleSlash: () => void,
+  deps: React.DependencyList
 ): React.KeyboardEventHandler<HTMLDivElement> => {
   return useCallback(
     (event: React.KeyboardEvent) => {
       if (vert) {
         // remap arrow keys on vertical writing mode
-        if (event.key === 'ArrowRight' || event.key == 'ArrowLeft') {
+        if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') {
           event.preventDefault()
           Editor.normalize(editor, { force: true })
           const dir = event.key === 'ArrowLeft' ? 'forward' : 'backward'
@@ -172,9 +259,24 @@ const useOnKeyDown = (
           })
           return
         }
+
+        // NOTE: This avoids sync error
+        // TODO:
+        if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+          event.preventDefault()
+          const reverse = event.key === 'ArrowUp'
+          Transforms.move(editor, { unit: 'offset', reverse })
+        }
+      }
+
+      if (event.key === '/' && event.ctrlKey) {
+        event.preventDefault()
+        // toggle
+        toggleSlash()
+        return
       }
     },
-    [editor, vert]
+    [editor, vert, ...deps]
   )
 }
 
@@ -185,24 +287,146 @@ export enum WritingDirection {
 
 export enum AppearPolicy {
   ByParagraph,
-  ByCharacter
-  // Rich
+  ByCharacter,
+  Rich,
+  ShowAll
 }
 
 /** Properties of {@link VedEditor}. */
 export type VedEditorProps = {
   readonly dir: WritingDirection
   readonly appearPolicy: AppearPolicy
+  readonly setAppearPolicy: (_: AppearPolicy) => void
 }
 
-export const VedEditor = ({ dir, appearPolicy }: VedEditorProps): React.JSX.Element => {
+const nodeToPlainText = (node: Node): string => {
+  if (Element.isElement(node)) {
+    const element = node as VedElement
+    switch (element.type) {
+      case 'Ruby':
+        return 'THIS IS a ruby! TODO: retrieve the text!'
+      default:
+        return Node.string(element)
+    }
+  }
+
+  return Node.string(node)
+}
+
+const unformatBuffer = (editor: Editor) => {
+  editor.children.forEach((node, iNode) => {
+    const text = nodeToPlainText(node)
+    const path = [iNode]
+    Transforms.removeNodes(editor, { at: path })
+    Transforms.insertNodes(editor, { children: [{ text }] }, { at: path })
+  })
+}
+
+const formatBuffer = (editor: Editor) => {
+  // the `element` must be just under root
+  editor.children.forEach((underRoot, iRoot) => {
+    if (!Element.isElement(underRoot)) {
+      return
+    }
+
+    underRoot.children.forEach((node, iChild) => {
+      // TODO: handle non-leaf nodes
+      if (!Text.isText(node)) {
+        return
+      }
+
+      const path = [iRoot, iChild]
+      const formats = parseFormats(node.text)
+      for (let i = formats.length - 1; i >= 0; i--) {
+        const fullText = Editor.string(editor, path)
+        const text = fullText.substring(formats[i].text[0], formats[i].text[1])
+        const rubyText = fullText.substring(formats[i].rubyText[0], formats[i].rubyText[1])
+
+        // wrap the text
+        const rubyElement = {
+          type: 'Ruby',
+          children: [
+            { text }
+            // { type: 'Rt', text: rubyText }
+          ]
+        }
+
+        // // replace the ruby format with body text
+        // Transforms.insertText(editor, text, {
+        //   at: {
+        //     anchor: { path, offset: formats[i].delimFront[0] },
+        //     focus: { path, offset: formats[i].delimEnd[1] }
+        //   }
+        // })
+
+        // Transforms.wrapNodes(editor, rubyElement, {
+        //   at: {
+        //     anchor: { path, offset: formats[i].delimFront[0] },
+        //     focus: { path, offset: formats[i].delimFront[0] + text.length }
+        //   },
+        //   split: true
+        // })
+
+        Transforms.insertNodes(
+          editor,
+          rubyElement,
+          // { children: [{ text: 'go' }] },
+          {
+            at: {
+              anchor: { path, offset: formats[i].delimFront[0] },
+              focus: { path, offset: formats[i].delimEnd[1] }
+            }
+          }
+        )
+
+        // what does this do?
+        // Transforms.collapse(editor, { edge: 'end' })
+      }
+    })
+  })
+}
+
+const withInlines = (editor: Editor) => {
+  const { isInline } = editor
+
+  editor.isInline = (element: VedElement) =>
+    (element.type !== undefined && ['Ruby'].includes(element.type)) || isInline(element)
+
+  return editor
+}
+
+export const VedEditor = ({
+  dir,
+  appearPolicy,
+  setAppearPolicy
+}: VedEditorProps): React.JSX.Element => {
   // TODO: Should use `useMemo` as in hovering toolbar example?
-  const [editor] = useState(() => withReact(withHistory(createEditor())))
+  const [editor] = useState(() => withInlines(withReact(withHistory(createEditor()))))
   const initialValue = [{ type: 'paragraph', children: [{ text: '' }] }]
   const decorate = useDecorate(editor, appearPolicy)
-  const renderLeaf = useCallback((props: RenderLeafProps) => <Leaf {...props} />, [])
+  const renderLeaf = useCallback((props: RenderLeafProps) => <VedLeaf {...props} />, [appearPolicy])
+  const renderElement = useCallback(
+    (props: RenderElementProps) => <VedElement {...props} />,
+    [appearPolicy]
+  )
   const vert = dir === WritingDirection.Vertical
-  const onKeyDown = useOnKeyDown(editor, vert)
+
+  const onKeyDown = useOnKeyDown(
+    editor,
+    vert,
+    () => {
+      if (appearPolicy === AppearPolicy.Rich) {
+        console.log('unformat buffer')
+        unformatBuffer(editor)
+        setAppearPolicy(AppearPolicy.ShowAll)
+      } else {
+        console.log('format buffer')
+        formatBuffer(editor)
+        setAppearPolicy(AppearPolicy.Rich)
+      }
+    },
+    [appearPolicy]
+  )
 
   return (
     <div className={clsx('ved-editor', vert && 'vert-mode', vert && 'multi-col-mode')}>
@@ -213,6 +437,7 @@ export const VedEditor = ({ dir, appearPolicy }: VedEditorProps): React.JSX.Elem
           className={clsx('ved-editor-content', vert && 'vert-mode', vert && 'multi-col-mode')}
           decorate={decorate}
           renderLeaf={renderLeaf}
+          renderElement={renderElement}
           onKeyDown={onKeyDown}
         />
       </Slate>
