670f5d24f040b77db2969055730f40d1d7331b70
toyboot4e <toyboot4e@gmail.com>
2026-01-17 03:21:26 +0900

    fix: follow the electron-vite migration guide
    https://electron-vite.org/guide/migration

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,13 +1,13 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import { defineConfig } from 'electron-vite';
 import { resolve } from 'path';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    //
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
+    //
   },
   renderer: {
     resolve: {
