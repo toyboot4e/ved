885c7919d9e910ecb69db14994eb466ef4eb4cdd
toyboot4e <toyboot4e@gmail.com>
2026-03-31 23:08:57 +0900

    dev: remove `flake-utils`

--- flake.nix
+++ flake.nix
@@ -3,62 +3,65 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        deps = with pkgs; lib.optionals stdenvNoCC.isLinux [
-          glib
-          util-linux
-          nss
-          nspr
-          dbus
-          atk
-          cups
-          gtk3
-          pango
-          cairo
-          # TODO: support Wayland?
-          xorg.libX11
-          xorg.libXcomposite
-          xorg.libXdamage
-          xorg.libXext
-          xorg.libXfixes
-          xorg.libXrandr
-          xorg.libxcb
-          libgbm
-          expat
-          libxkbcommon
-          alsa-lib
-          libdrm
-          libGL
-        ] ++ lib.optionals stdenvNoCC.isDarwin [
-          #
-        ];
-      in
-      {
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: f nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (
+        pkgs:
+        let
+          deps = with pkgs; lib.optionals stdenvNoCC.isLinux [
+            glib
+            util-linux
+            nss
+            nspr
+            dbus
+            atk
+            cups
+            gtk3
+            pango
+            cairo
+            # TODO: support Wayland?
+            xorg.libX11
+            xorg.libXcomposite
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXrandr
+            xorg.libxcb
+            libgbm
+            expat
+            libxkbcommon
+            alsa-lib
+            libdrm
+            libGL
+          ] ++ lib.optionals stdenvNoCC.isDarwin [
+            #
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
               pkg-config
               # nix-ld
             ] ++ deps;
 
-            packages = [
-              nodePackages.prettier
+            packages = with pkgs; [
+              biome
             ];
             shellHook = ''
               export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath deps}"
             '';
           };
-      }
-    );
+        }
+      );
+    };
 }
